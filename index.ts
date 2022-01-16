8c5b2532ca8c30656abb64fd2f7bd445b2bdf2da
Aravindakrishnan <aravindapks@gmail.com>
2022-01-16 15:59:12 +0530

    Added Block feature

--- index.ts
+++ index.ts
@@ -17,6 +17,10 @@ const transactions = new Transactions();
 const user1 = new User("Address 1",["one","two","three"]);
 const user2 = new User("Address 2",["one","two","three"]);
 const user3 = new User("Address 3",["one","two","three"]);
+const user4 = new User("Address 4",["one","two","three"]);
+const user5 = new User("Address 5",["one","two","three"]);
+const user6 = new User("Address 6",["one","two","three"]);
+
 
 // Todo: Adding the user into users
 // .addUser(user : User) method
@@ -24,6 +28,10 @@ const user3 = new User("Address 3",["one","two","three"]);
 users.addUser(user1);
 users.addUser(user2);
 users.addUser(user3);
+users.addUser(user4);
+users.addUser(user5);
+users.addUser(user6);
+
 
 // Todo: Depositing Fiat funds for User 
 // Fiat { symbol : string, amount : number }
@@ -31,6 +39,10 @@ users.addUser(user3);
 user1.depositFiat(new Fiat("INR",1000000));
 user2.depositFiat(new Fiat("INR",42341));
 user3.depositFiat(new Fiat("INR",32341));
+user4.depositFiat(new Fiat("INR",13234));
+user5.depositFiat(new Fiat("INR",3321));
+user6.depositFiat(new Fiat("INR",120));
+
 
 // Todo: Viewing Fiat Balance
 // .viewFiatBalance() : void -> logs [fiat balance]
@@ -38,6 +50,9 @@ user3.depositFiat(new Fiat("INR",32341));
 user1.viewFiatBalance();
 user2.viewFiatBalance();
 user3.viewFiatBalance();
+user4.viewFiatBalance();
+user5.viewFiatBalance();
+user6.viewFiatBalance();
 
 
 // Todo: Purchasing Cryptocurrencies
@@ -46,6 +61,10 @@ user3.viewFiatBalance();
 user1.purchaseCrypto("BTC","INR",999999);
 user2.purchaseCrypto("SHIB","INR",12000);
 user3.purchaseCrypto("SOL","INR",12999);
+user4.purchaseCrypto("ETH","INR",23132);
+user5.purchaseCrypto("DOGE","INR",1000);
+user6.purchaseCrypto("SHIB","INR",100);
+
 
 // Todo: Viewing Crypto Balance
 // .viewCryptoBalance() : void -> logs [crypto balance]
@@ -53,6 +72,10 @@ user3.purchaseCrypto("SOL","INR",12999);
 user1.viewCryptoBalance();
 user2.viewCryptoBalance();
 user3.viewCryptoBalance();
+user4.viewCryptoBalance();
+user5.viewCryptoBalance();
+user6.viewCryptoBalance();
+
 
 // Todo: WithdrawCoin means sending coins to another account with the public address 
 /*
@@ -67,14 +90,26 @@ user3.viewCryptoBalance();
 
 user1.withdrawCoin(users.users,transactions,user2.publicAddress,new Coin("BTC",0.04,user1.publicAddress));
 user2.withdrawCoin(users.users,transactions,user3.publicAddress,new Coin("SHIB",1000,user2.publicAddress));
-user3.withdrawCoin(users.users,transactions,user1.publicAddress,new Coin("SOL",0.5,user3.publicAddress));
+user3.withdrawCoin(users.users,transactions,user4.publicAddress,new Coin("SOL",0.5,user3.publicAddress));
+user4.withdrawCoin(users.users,transactions,user5.publicAddress,new Coin("ETH",0.000001,user4.publicAddress));
+user5.withdrawCoin(users.users,transactions,user6.publicAddress,new Coin("DOGE",2,user5.publicAddress));
+user6.withdrawCoin(users.users,transactions,user1.publicAddress,new Coin("SHIB",1500,user6.publicAddress));
 
 // Todo: getTransactions
 // .getTransactions() returns the transaction[]
 console.log(user1.getTransactions());
 console.log(user2.getTransactions());
 console.log(user3.getTransactions());
+console.log(user4.getTransactions());
+console.log(user5.getTransactions());
+console.log(user6.getTransactions());
 
-// coming soon...
-const block1 = new Block("0".repeat(16),transactions.getTransactions());
+console.log(transactions.getTransactions().length)
 
+// Blocks of transactions 
+
+const block1 = new Block("0".repeat(16),transactions.getTransactions().splice(0,3));
+const block2 = new Block(block1.getBlockHash(),transactions.getTransactions().splice(3));
+
+console.log(block1);
+console.log(block2);
